4d0b24edcd03f77fccf6807ec75326e0c71237e5
Srinivasan Duraiswamy <srindura@cisco.com>
2022-02-27 10:12:18 -0700

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Please RelaaXe Yourself !
+          Please Relaaxe Yourself !
         </p>
         <p>
           We are building it for you :)
